3477dad1fc426df3dbdf65b592132beaa2963a9a
Marc Wils <marc.wils@vecozo.nl>
2023-10-12 12:27:24 +0200

    publish github pages

--- Program.cs
+++ Program.cs
@@ -1,8 +1,5 @@
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-builder.Services.AddRazorPages();
-
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -15,11 +12,4 @@ if (!app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 app.UseStaticFiles();
-
-app.UseRouting();
-
-app.UseAuthorization();
-
-app.MapRazorPages();
-
 app.Run();
