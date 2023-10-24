c5e29585b3ceebe2241ab402f7b1e7c7d206d4a4
Veltman, Kars <Kars.Veltman@vecozo.nl>
2023-10-24 16:38:09 +0200

    Linkjes werkend gemaakt

--- src/Zippy.Site/Program.cs
+++ src/Zippy.Site/Program.cs
@@ -1,11 +1,24 @@
-using Zippy.Site;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Net.Http;
+using System.Threading.Tasks;
 
-var builder = WebAssemblyHostBuilder.CreateDefault(args);
-builder.RootComponents.Add<App>("#app");
-builder.RootComponents.Add<HeadOutlet>("head::after");
+namespace Zippy.Site
+{
+    static class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            var builder = WebAssemblyHostBuilder.CreateDefault(args);
+            builder.RootComponents.Add<App>("#app");
+            builder.RootComponents.Add<HeadOutlet>("head::after");
 
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+            builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
-await builder.Build().RunAsync();
+            await builder.Build().RunAsync();
+        }
+    }
+
+}
